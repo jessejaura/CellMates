824649abc241ef61799a1fdcf8725f831cedcae3
Ana <ana.damnjanovic@mail.utoronto.ca>
2019-02-06 18:35:13 -0500

    cleaned up variable names and added teleport to membrane

--- Meld/Assets/Scripts/move3.cs
+++ Meld/Assets/Scripts/move3.cs
@@ -2,18 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NVIDIA.Flex;
 
 public class move3 : MonoBehaviour
 {
     public float speed = 1;
     public float topSpeed = 2;
-    public float maxSeperation = 2;
+    public float maxSeparation = 2;
     public float jump = 10;
-    private Rigidbody cube;
+    private Rigidbody rb;
     Vector3 movement;
     private GameObject player1;
     private GameObject player2;
     private GameObject membrane;
+    private FlexClothActor membraneCloth;
 
     public string inputHorizontal;
     public string inputVertical;
@@ -27,17 +29,18 @@ public class move3 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        cube = GetComponent<Rigidbody>();
-        cube.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
         player1 = GameObject.FindWithTag(player1Tag);
         player2 = GameObject.FindWithTag(player2Tag);
         membrane = GameObject.FindWithTag(membraneTag);
+        membraneCloth = GameObject.FindWithTag(membraneTag).GetComponent<FlexClothActor>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (true || (Vector3.Distance(player1.transform.position, player2.transform.position) <= maxSeperation))
+        if (true || (Vector3.Distance(player1.transform.position, player2.transform.position) <= maxSeparation))
         {
 
             float horizontal = Input.GetAxis(inputHorizontal) * 20;
@@ -45,7 +48,7 @@ public class move3 : MonoBehaviour
 
             Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
 
-            //cube.AddForce(movement * speed);
+            //rb.AddForce(movement * speed);
 
 
             // Controlling movement speed on the XZ plane
@@ -53,59 +56,77 @@ public class move3 : MonoBehaviour
             if (movement.magnitude > topSpeed)
                 movement = movement.normalized * topSpeed;
             // This is to preserve Y movement so that gravity affects it properly
-            movement.y = cube.velocity.y;
+            movement.y = rb.velocity.y;
 
-            cube.velocity = movement;
+            rb.velocity = movement;
 
-            //cube.AddForce(movement);
-            //Vector3 vel = cube.velocity;
+            //rb.AddForce(movement);
+            //Vector3 vel = rb.velocity;
             //vel.x = horizontal;
             //vel.y = vertical;
-            //        cube.velocity = vel;
+            //        rb.velocity = vel;
         }
 
         // Checking if we are above the ground, stop moving so we don't sink out of the blob
         var ray = new Ray(transform.position, Vector3.down);
         bool grounded = Physics.Raycast(ray, raycastDistance);
         
-        var rAy = new Ray(player1.transform.position, Vector3.down);
-        bool player1grounded = Physics.Raycast(rAy, raycastDistance);
-        var raY = new Ray(player2.transform.position, Vector3.down);
-        bool player2grounded = Physics.Raycast(raY, raycastDistance);
+        var player1Ray = new Ray(player1.transform.position, Vector3.down);
+        bool player1grounded = Physics.Raycast(player1Ray, raycastDistance);
+        var player2Ray = new Ray(player2.transform.position, Vector3.down);
+        bool player2grounded = Physics.Raycast(player2Ray, raycastDistance);
         grounded = player1grounded || player2grounded;
         
         // This should prevent the player from sinking to the ground
-        cube.useGravity = !grounded;
+        rb.useGravity = !grounded;
         // This should extra prevent the player from sinking
         if (grounded)
         {
-            var temp = cube.velocity;
-            temp.y = Math.Max(0, temp.y);
-            cube.velocity = temp;
+            var minVelocity = rb.velocity;
+            minVelocity.y = Math.Max(0, minVelocity.y);
+            rb.velocity = minVelocity;
         }
         
-        Vector3 player1position = player1.transform.position;
-        Vector3 player2position = player2.transform.position;
-        player1position.y = 0;
-        player2position.y = 0;
+        Vector3 player1Position = player1.transform.position;
+        Vector3 player2Position = player2.transform.position;
+        player1Position.y = 0;
+        player2Position.y = 0;
 
-        if (Vector3.Distance(player1position, player2position) > maxSeperation || !grounded)
+        float playerDistance = Vector3.Distance(player1Position, player2Position);
+        Vector3 avg = (player1.transform.position + player2.transform.position) / 2;
+
+        // start shaking the membrane by pulling players together gently when they're close to max separation
+        if ((maxSeparation - 1.5) <= playerDistance && playerDistance < maxSeparation)
         {
-            Vector3 avg = (player1.transform.position + player2.transform.position) / 2;
-            //player1.transform.position = avg;
-            //player2.transform.position = avg;
-            //membrane.transform.position = avg;
-            
-            if (grounded)
-                avg.y = jump;
+            jumpMagnitude = 10;
+            if ((maxSeparation - 0.5) <= playerDistance && playerDistance < maxSeparation)
+            {
+                jumpMagnitude = 15;
+            }
 
             player1.GetComponent<Rigidbody>().AddForce((avg - player1.transform.position).normalized * jumpMagnitude);
             player2.GetComponent<Rigidbody>().AddForce((avg - player2.transform.position).normalized * jumpMagnitude);
+            //membrane.transform.position = membraneAvg;
 
-            //player1.transform.position = avg;
-            //player2.transform.position = avg;
-            
-            //membrane.transform.position = avg;
+        }
+        if (playerDistance > maxSeparation || !grounded)
+        {
+
+            if (grounded)
+            {
+                avg.y = jump;
+            }
+
+            jumpMagnitude = 150;
+            player1.GetComponent<Rigidbody>().AddForce((avg - player1.transform.position).normalized * jumpMagnitude);
+            player2.GetComponent<Rigidbody>().AddForce((avg - player2.transform.position).normalized * jumpMagnitude);
+
+            Vector3 membraneAvg = avg + 0.25f * Vector3.up;
+            membrane.transform.localScale = new Vector3(1.75f, 1.25f, 1.75f);
+            membraneCloth.Teleport(membraneAvg, membrane.transform.rotation);
+            membrane.transform.position = membraneAvg;
+
+
         }
     }
 }
